00da88f9798e968299249f22780f9a9bac8f2d1a
mcps5601 <mcps5601@gmail.com>
2020-10-13 14:43:58 +0800

    add save_params.py

---
+++ save_params.py
@@ -0,0 +1,19 @@
+import json
+
+# Reference: CS230
+# https://github.com/cs230-stanford/cs230-code-examples/blob/master/pytorch/nlp/utils.py
+
+
+def save_dict_to_json(dict_of_params, json_path):
+    """Saves dict of floats in json file
+    Args:
+        d: (dict) of float-castable values (np.float, int, float, etc.)
+        json_path: (string) path to json file
+    Return:
+        a saved json file containing hyperparameters
+    """
+
+    with open(json_path, 'w') as f:
+        # We need to convert the values to float for json (it doesn't accept np.array, np.float, )
+        dict_of_params = {k: float(v) for k, v in dict_of_params.items()}
+        json.dump(dict_of_params, f, indent=4)
