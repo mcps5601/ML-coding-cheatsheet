467d2339430069b4fd3e1d2b62acc2a29f1df84b
mcps5601 <mcps5601@gmail.com>
2020-10-14 11:05:46 +0800

    add tf random seed

--- set_seed.py
+++ set_seed.py
@@ -1,10 +1,9 @@
-import os
-import random
-import numpy as np
+##### PyTorch #####
 import torch
+import os, random
+import numpy as np
 
 # This function can be successfully used in PyTorch v.1.6.0 for reproducibility.
-
 def set_seed(seed):
     """
     Args:
@@ -20,3 +19,32 @@ def set_seed(seed):
         #torch.cuda.manual_seed_all(seed)  # if using more than one GPUs
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
+
+
+##### TensorFlow1 #####
+import tensorflow.compat.v1 as tf
+
+# This function can be successfully used in TensorFlow 1.x for reproducibility.
+def tf1_set_seed(seed):
+    """
+    Args:
+        seed: an integer number to initialize a pseudorandom number generator
+    """
+    tf.random.set_seed
+
+
+##### TensorFlow2 #####
+import tensorflow as tf
+import os, random
+import numpy as np
+
+# This function can be successfully used in TensorFlow 2.x and Keras for reproducibility.
+def tf2_set_seed(seed):
+    """
+    Args:
+        seed: an integer number to initialize a pseudorandom number generator
+    """
+    tf.random.set_seed(seed_value)
+    os.environ['PYTHONHASHSEED']=str(seed_value)
+    random.seed(seed_value)
+    np.random.seed(seed_value)
